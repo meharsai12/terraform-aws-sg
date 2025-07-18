7f58ee99d548196e7dd56049409df642e6d9b625
Ravikanti Sai <RavikantiS@emids.com>
2025-07-18 13:32:43 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "main" {
+  name        = var.sg_name
+  description = var.sg_description
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    local.common_tags , var.sg_tags,
+    {
+        Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "sg_name" {
+  
+}
+
+variable "sg_description" {
+  
+}
+
+
+
+variable "vpc_id" {
+  
+}
+
+
+variable "sg_tags" {
+ default = {} 
+}
+
+
+variable "environment" {
+  
+}
+
+
+variable "project" {
+  
+}
+
+
